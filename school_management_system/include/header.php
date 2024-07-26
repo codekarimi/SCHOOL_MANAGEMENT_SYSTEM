7d046fb13ace7f478f3913906878152a263b0532
zeuss254 <ckarimi676@gmail.com>
2024-07-27 00:17:07 +0300

    Navbar Navigation

---
+++ school_management_system/include/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap cdn link  -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <!-- jquery cdn link -->
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+
+    <!-- jquery Google CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+    <!-- fontawesome cdn link  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
+
+    <!-- fontawesome cdn for all assests -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <title>School Mangament System</title>
+</head>
+
+<body>
+
+    <header class="col-md-12">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8">
+                <h3 class="mx-3">SMS project</h3>
+            </div>
+            <div class="col-md-4">
+                <i class="fa-brands fa-whatsapp"></i>
+                <span>+23456556566</span>
+            </div>
+        </div>
+    </header>
+
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand text-info">School Mangament System</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbar">
+                <div class="me-auto"></div>
+                <ul class="navbar-nav mx-5">
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Register</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Login</a>
+                        <ul class="dropdown-menu" id="navbarDropdown">
+                            <li class="nav-item"><a href="./adminlogin.php" class="dropdown-item">Admin</a></li>
+                            <li class="nav-item"><a href="#" class="dropdown-item">Lecture</a></li>
+                            <li class="nav-item"><a href="#" class="dropdown-item">Student</a></li>
+                            <li class="nav-item"><a href="#" class="dropdown-item">Parent</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- You need to make sure you're using the Bootstrap 5 CDN that includes Popper:  fo downdown to work-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
