2e6759588c7eaa1c7c0b820c24d48ead28a71e55
zeuss254 <ckarimi676@gmail.com>
2024-07-27 00:16:08 +0300

    Footer Page

---
+++ school_management_system/include/footer.php
@@ -0,0 +1,7 @@
+<div class="col-md-12 bg-light my-2" style="position: absolute; bottom:0;">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-5">
+            <h5 class="text-center">Subscribe to my chaneel</h5>
+        </div>
+    </div>
+</div>
